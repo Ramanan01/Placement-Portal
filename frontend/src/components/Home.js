fa38b9b43c878038d1640e0f5a4c72df49d99d92
Ramya-1808 <ramyanmsr@gmail.com>
2022-05-06 10:56:34 +0530

    commit bgimg

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const theme = createTheme();
 const Home = () => {
     const navigate = useNavigate()
   return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          ></Box>
     <div>
-        <b>Home</b>
+       <Typography component="h1" variant="h5">
+          Home
+        </Typography>
         <div>
             <button onClick={()=>{
                navigate('\login') 
@@ -15,6 +57,9 @@ const Home = () => {
             }}>Organization</button>
         </div>
     </div>
+    </Grid>
+    </Grid>
+    </ThemeProvider>
   )
 }
 
